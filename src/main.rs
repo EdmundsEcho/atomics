ce4e4b352d318c7fcf7d512e750a2388bfd4670d
Edmund Cape <edmund@Edmunds-MacBook-Pro.local>
2023-02-25 16:39:26 -0500

    Threads can borrow something that leaks - never dropped

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,25 @@ fn main() {
     // Prevent any one thread from owning a value using Static
     // This value exists even before main is called.
     //
+    // 🔑 Normally we cannot borrow values because a thread can live as long as "forever" (from
+    // main's perpsective).  Static ensure what is being borrowed will in fact live forever.
+    //
+    // Note: borrowing is the only way to share a resource.
+    //
     static X: [i32; 3] = [1, 2, 3];
 
     let t1 = thread::spawn(|| dbg!(&X));
     let t2 = thread::spawn(|| dbg!(&X));
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+
+    //
+    // Another way to accomplish the same, memory leaking.
+    //
+    let x: &'static [i32; 3] = Box::leak(Box::new([1, 2, 3]));
+    let t1 = thread::spawn(move || dbg!(x)); // copy a borrow
+    let t2 = thread::spawn(move || dbg!(x)); // copy a borrow
 
     t1.join().unwrap();
     t2.join().unwrap();
